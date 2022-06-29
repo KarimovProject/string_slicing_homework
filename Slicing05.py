48d3e602ae4dc75870f62a25ff03ec818d1067c1
KarimovProject <abdullatifkarimov772@gmail.com>
2022-06-29 15:13:05 +0500

    5 misol

--- Slicing05.py
+++ Slicing05.py
@@ -1,10 +1,14 @@
 def main(s,n):
     """
-    The s string variable is given. return n characters from the end.
+    The s string variable is given. return n characters from the beginning.
     Args:
         s(str): parameter
         n(int): parameter
     Returns:
         str: answer
     """
-    return
+    if n>0:
+        return s[-n:]
+    else:
+        return "notog'ri qiymat kiritildi"
+print(main("salom", 2))
